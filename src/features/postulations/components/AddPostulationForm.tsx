d00cfc98d3e806cd041c30d5dd77995b330febf3
Ale Weber <alexis.weber@mailfence.com>
2025-03-09 22:15:58 -0300

    add AddPostulationForm component

---
+++ src/features/postulations/components/AddPostulationForm.tsx
@@ -0,0 +1,21 @@
+export const AddPostulationsForm: React.FC = () => {
+  return (
+    <>
+      <form>
+        <label></label>
+        <input type="text" placeholder="posición" />
+        <label></label>
+        <input type="text" placeholder="status" />
+        <label></label>
+        <input type="text" placeholder="fecha de postulación" />
+        <label></label>
+        <input type="text" placeholder="fecha de respuesta" />
+        <label></label>
+        <input type="text" placeholder="link" />
+        <label></label>
+        <input type="text" placeholder="" />
+        <checkbox></checkbox>
+      </form>
+    </>
+  );
+};
