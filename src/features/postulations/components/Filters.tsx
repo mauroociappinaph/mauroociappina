4cea1cb774cfbd2665130c278c3de871d87d78b6
Ale Weber <alexis.weber@mailfence.com>
2025-03-23 19:57:48 -0300

    update filter posttion

--- src/features/postulations/components/Filters.tsx
+++ src/features/postulations/components/Filters.tsx
@@ -3,34 +3,37 @@ import styles from "../styles/Filters.module.css";
 
 export const Filters: React.FC = () => {
   return (
-    <div className={styles.filters}>
+    <div className={styles.content}>
       <h2 className={styles.h2}>Filters</h2>;
-      {/* Aquí puedes agregar filtros según lo que necesites */}
       <div className={styles.filters}>
-        <label className={styles.labelTitle} htmlFor="status-filter">
-          Status
-        </label>
-        <select id="status-filter" name="status-filter">
-          <option className={styles.optionTitle} value="all">
-            All
-          </option>
-          <option className={styles.optionTitle} value="pending">
-            Pendiente
-          </option>
-          <option className={styles.optionTitle} value="approved">
-            Aprobado
-          </option>
-          <option className={styles.optionTitle} value="rejected">
-            Rechazado
-          </option>
-        </select>
-        <label className={styles.labelTitle}>Posición</label>
-        <select id="position-filter" name="position-filter">
-          <option className={styles.optionTitle} value="todos">
-            Todos
-          </option>
-          <option>{}</option>
-        </select>
+        <div className="filter">
+          <label className={styles.labelTitle} htmlFor="status-filter">
+            Estado :
+          </label>
+          <select id="status-filter" name="status-filter">
+            <option className={styles.optionTitle} value="all">
+              All
+            </option>
+            <option className={styles.optionTitle} value="pending">
+              Pendiente
+            </option>
+            <option className={styles.optionTitle} value="approved">
+              Aprobado
+            </option>
+            <option className={styles.optionTitle} value="rejected">
+              Rechazado
+            </option>
+          </select>
+        </div>
+        <div className="filter">
+          <label className={styles.labelTitle}>Posición :</label>
+          <select id="position-filter" name="position-filter">
+            <option className={styles.optionTitle} value="todos">
+              Todos
+            </option>
+            <option>{}</option>
+          </select>
+        </div>
       </div>
     </div>
   );
