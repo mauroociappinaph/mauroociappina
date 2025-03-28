1d03ecf8665ab1bf814f0eb399cd0ac289bb7d82
Ale Weber <alexis.weber@mailfence.com>
2025-03-28 15:26:34 -0300

    fix getPostulationDetailService

--- src/features/postulations/services/postulationsService.ts
+++ src/features/postulations/services/postulationsService.ts
@@ -18,10 +18,22 @@ export const fetchPostulations = async (
 export const createPostulation = async (postulationData: Postulation) => {
   try {
     const response = await axios.post(`${api}/postulation/`, postulationData);
-    console.log("createPostulation response", response.data.result);
     return response.data.result;
   } catch (error) {
     console.error("Error creating postulation:", error);
     throw error;
   }
 };
+
+export const getPostulationDetail = async (postId: string) => {
+  try {
+    const response = await axios.get(`${api}/postulation/${postId}`);
+    console.log("response", response.data.result);
+    return response.data.result.postulation;
+  } catch (error) {
+    console.error("Error fetching postulation detail:", error);
+    throw error;
+  }
+};
+
+export const updatePostulation = async (postulationData: Postulation) => {};
