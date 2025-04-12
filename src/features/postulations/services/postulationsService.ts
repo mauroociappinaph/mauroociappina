0f02e4b8d66a2196e9e0897659af6b42f6a2a3b0
Ale Weber <alexis.weber@mailfence.com>
2025-04-11 23:14:45 -0300

    FIX: update server endpoints

--- src/features/postulations/services/postulationsService.ts
+++ src/features/postulations/services/postulationsService.ts
@@ -6,21 +6,25 @@ const api = import.meta.env.VITE_API_URL;
 export const fetchPostulations = async (
   userId: string,
 ): Promise<Postulation[]> => {
-  const response = await axios.get(`${api}/postulations/${userId}`);
-  return response.data.result.postulations;
+  try {
+    const response = await axios.get(`${api}/postulations/user/${userId}`);
+    return response.data.result.postulations;
+  } catch (error) {
+    console.error("Error fetching postulations:", error);
+    throw error;
+  }
 };
 
 export const createPostulation = async (
   postulationData: CreatePostulationDTO,
 ): Promise<Postulation> => {
-  const response = await axios.post(`${api}/postulation`, postulationData);
+  const response = await axios.post(`${api}/postulations`, postulationData);
   return response.data.result;
 };
 
 export const getPostulationDetail = async (postId: string) => {
   try {
-    const response = await axios.get(`${api}/postulation/${postId}`);
-    console.log("response", response.data.result);
+    const response = await axios.get(`${api}/postulations/${postId}`);
     return response.data.result.postulation;
   } catch (error) {
     console.error("Error fetching postulation detail:", error);
@@ -28,4 +32,6 @@ export const getPostulationDetail = async (postId: string) => {
   }
 };
 
-export const updatePostulation = async (postulationData: Postulation) => {};
+export const updatePostulation = async (postulationData: Postulation) => {
+  console.log("Updating postulation:", postulationData);
+};
